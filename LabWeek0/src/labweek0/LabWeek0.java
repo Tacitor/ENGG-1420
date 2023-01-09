d6404b5c0ca7c20d00b8c8c457c5340d96516c37
Tacitor <lukasjkrampitz@gmail.com>
2023-01-09 11:44:37 -0500

    Add project from the Lab form Week 0

---
+++ LabWeek0/src/labweek0/LabWeek0.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labweek0;
+
+/**
+ *
+ * @author Lukas
+ */
+public class LabWeek0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("First Java Program for this course" + "\nMaking sure the IDE and compile environment is all setup");
+    }
+    
+}
