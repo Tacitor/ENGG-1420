c31f94f219b73f173a7b148d5f75f5f502520871
Tacitor <lukasjkrampitz@gmail.com>
2023-01-10 16:21:40 -0500

    Finish part 1

--- Assignment1/assignment1_part1.c
+++ Assignment1/assignment1_part1.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+#define SIZE 4
+
 int main(void) {
 
-    printf("Le Test");
+    char num_str[SIZE + 1]; //A char for 4 numbers and a delimeter at the end
+
+    printf("Please enter a four-digit number: ");
+    scanf("%s", num_str); //read in the number as a string of characters
+
+    //loop through the number backwards
+    for (int i = 0; i < SIZE; i++) {
+        printf("%c", num_str[SIZE - 1 - i]);
+    }
+
+    //print a new line to ensure good formatting
+    printf("\n");
 
     return 0;
 }
