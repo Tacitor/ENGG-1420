95014e87763ee4edcd8a2186ced9cb90164bad77
Tacitor <lukasjkrampitz@gmail.com>
2023-01-10 17:00:25 -0500

    Finsih 4

---
+++ Assignment1/assignment1_part4.c
@@ -0,0 +1,40 @@
+/**
+ * Lukas Krampitz
+ * 10 Jan 2023
+ * assignment1_part4
+ */
+#include <stdio.h>
+
+long long int divisorSum(int);
+
+int main(void)
+{
+
+    // Testing the function
+    printf("Ans: %lld", divisorSum(6));
+
+    return 0;
+}
+
+/**
+ * Takes an integer n as a parameter and calculates and returns the sum of all
+ * divisors of n.
+ */
+long long int divisorSum(int num)
+{
+    int sum = 0; // an accumulator to hold the sum
+
+    // loop through all the numbers including an less than num and check if it is a factor (divisor)
+    for (int i = num; i >= 1; i--)
+    {
+        // check for perfect divisability (no remainder)
+        if (num % i == 0)
+        {
+            // then sum it up
+            sum += i;
+        }
+    }
+
+    // return the result
+    return sum;
+}
