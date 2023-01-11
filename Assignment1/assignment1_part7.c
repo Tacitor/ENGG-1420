61ef3096194be612c8888b008d1b5bb76b28982a
Tacitor <lukasjkrampitz@gmail.com>
2023-01-10 21:39:37 -0500

    finish part 7

--- Assignment1/assignment1_part7.c
+++ Assignment1/assignment1_part7.c
@@ -14,13 +14,17 @@ typedef struct Date
 
 Date_t getNextDate(Date_t);
 void printDate(Date_t);
+int valComp(int num1, int num2);
 int compDate(Date_t, Date_t);
 
 int main(void)
 {
 
     // Testing the function
-    Date_t testDate1 = {31, 12, 2022};
+    Date_t testDate1 = {6, 3, 2004};
+    Date_t testDate2 = {27, 3, 2004};
+
+    printf("Result: %d", compDate(testDate1, testDate2));
 
     return 0;
 }
@@ -30,10 +34,53 @@ int main(void)
  */
 int compDate(Date_t date1, Date_t date2)
 {
-    int yearDiff, monthDiff, dayDiff; //The value differeneces between the members
+    int yearDiff, monthDiff, dayDiff; // The value differeneces between the members
+    int finalComp;                    // The end result of the comparison
 
+    // get the indevidual member comparisons
+    yearDiff = valComp(date1.year, date2.year);
+    monthDiff = valComp(date1.month, date2.month);
+    dayDiff = valComp(date1.day, date2.day);
 
-    return 5;
+    // check if the year is the same
+    if (yearDiff)
+    {
+        // if the year is not 0
+        finalComp = yearDiff; // then return the difference value for the year
+    }
+    else if (monthDiff) // if the year is the same then the month needs to be checked
+    {
+        // if the month comparison is not 0
+        finalComp = monthDiff; // return which month is bigger
+    }
+    else if (dayDiff) // if the month is the same then the day needs to be compared
+    {
+        // if the day is not 0
+        finalComp = dayDiff;
+    }
+    else
+    {
+        // if the rest are all false (0 value) then return 0 as they are all the same
+        finalComp = 0;
+    }
+
+    return finalComp;
+}
+
+/**
+ * Comapres two integers. Returns +1 if the first parameter is bigger than the second. Reurns -1 if the first parameter is less than the second. Returns 0if the same.
+ */
+int valComp(int num1, int num2)
+{
+
+    if (num1 == num2)
+    {
+        return 0;
+    }
+    else
+    {
+        return num1 > num2 ? 1 : -1;
+    }
 }
 
 /**
