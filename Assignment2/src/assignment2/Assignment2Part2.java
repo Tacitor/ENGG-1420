ecc065dd2a4092add8e972942afbbe7c5c6071ff
Tacitor <lukasjkrampitz@gmail.com>
2023-01-15 01:11:39 -0500

    add Assignment 2 Part 2

---
+++ Assignment2/src/assignment2/Assignment2Part2.java
@@ -0,0 +1,48 @@
+/*
+ * Lukas Krampitz - lkrampit
+ * Jan 15, 2023
+ * Assignment 2 Part 2
+ * Rewrite the Part 1 program using switch-case and for statements.
+ */
+package assignment2;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class Assignment2Part2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Vars
+        final int STOP_VAL = 10000; //The value the search should stop at. This value itself is not checked
+        boolean hasFactor; //keeps track if a factor has been found yet
+
+        //loop until the STOP_VAL is reached
+        for (int count = 2; count < STOP_VAL; count++) { //Start at 2 since 1 is not a prime.
+
+            //reset hasFactor to false for this new value
+            hasFactor = false;
+
+            //loop through all values of i less than the current value (count)
+            for (int i = 2; i < count; i++) { //skip 1 as it is always a factor
+
+                //test if i is a factor
+                switch (count % i) {
+                    case 0:
+                        hasFactor = true;
+                        break;
+                }
+            }
+
+            //print the value if it had no factors
+            if (!hasFactor) {
+                System.out.println(count);
+            }
+        }
+
+    }
+
+}
