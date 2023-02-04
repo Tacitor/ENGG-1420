0760fad9be4c7ad75c03fb300f9fb9e09493e945
Tacitor <lukasjkrampitz@gmail.com>
2023-02-04 13:40:35 -0500

    Add start of Assignment4Part1

---
+++ Assignment4Part1/src/assignment4part1/Assignment4Part1.java
@@ -0,0 +1,21 @@
+/*
+ * Lukas Krampitz
+ * Feb 4, 2023
+ * 
+ */
+package assignment4part1;
+
+/**
+ *
+ * @author Tacitor
+ */
+public class Assignment4Part1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
