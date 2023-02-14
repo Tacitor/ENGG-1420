609f645f7f4ab7481024a378c2c76130375740e4
Tacitor <lukasjkrampitz@gmail.com>
2023-02-14 12:06:12 -0500

    finish Lab session from week 6

--- LabWeek6/src/labweek6/LabWeek6.java
+++ LabWeek6/src/labweek6/LabWeek6.java
@@ -8,9 +8,18 @@ package labweek6;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class LabWeek6 extends Application {
@@ -21,9 +30,11 @@ public class LabWeek6 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("JavaFx is an arduous process!");
         Button btn = new Button();
+        Button btn2 = new Button();
         btn.setText("Say 'Hello World'");
+        btn2.setText("Say 'Hi'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
@@ -31,10 +42,40 @@ public class LabWeek6 extends Application {
                 System.out.println("Hello World!");
             }
         });
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hi");
+            }
+        });
+
+//        StackPane root = new StackPane();
+//        root.setAlignment(btn,Pos.BOTTOM_RIGHT);
+//        root.setAlignment(btn2,Pos.TOP_LEFT);
+//        root.getChildren().add(btn2);
+//        root.getChildren().add(btn);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahama", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+        
+        
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
